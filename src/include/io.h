aceae13bdc7c6cb9f3fb3acec5ffe24d0783e260
Llucs <104036495+Llucs@users.noreply.github.com>
2026-05-03 18:43:42 -0300

    Fix: Corrigir sintaxe de assembly inline em io.h

--- src/include/io.h
+++ src/include/io.h
@@ -4,12 +4,12 @@
 #include <stdint.h>
 
 static inline void outb(uint16_t port, uint8_t val) {
-    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+    asm volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) );
 }
 
 static inline uint8_t inb(uint16_t port) {
     uint8_t ret;
-    asm volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
+    asm volatile ( "inb %w1, %b0" : "=a"(ret) : "Nd"(port) );
     return ret;
 }
 
